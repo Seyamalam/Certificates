5ebe20335545de9917878e1f7b292f960bdcaadc
Seyamalam <seyamalam41@gmail.com>
2024-06-15 15:04:25 +0600

    Removed Scripts

--- a.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('README.md', 'r') as file:
-    content = file.read()
-
-content = re.sub(r'%20', '_', content)
-
-with open('README.md', 'w') as file:
-    file.write(content)
